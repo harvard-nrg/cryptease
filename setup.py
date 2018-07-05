45929e1b887b0eb1c3a6d1ad171d3f0ab8f5cf75
Timothy O'Keefe <timothy_okeefe@harvard.edu>
2018-07-05 15:43:53 -0400

    adding dependencies back to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
 
+requires = [
+    'cryptography',
+    'paramiko'
+]
+
 setup(name='encrypt',
       description='encrypt',
       author='Neuroinformatics Research Group',
       author_email='support@neuroinfo.org',
       url='http://neuroinformatics.harvard.edu/',
       packages=find_packages(),
+      install_requires=requires,
       scripts=[
         'scripts/crypt.py',
         'scripts/keyring.py'
